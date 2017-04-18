1eef0f082a7f3c519121695f7b5a72d25e7bfd3e
Bradley Gillick <bradleygillick@Bradleys-MacBook-Air.local>
2017-04-18 11:09:26 -0600

    Heroku Deployment

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 // Update with your config settings.
 
 module.exports = {
@@ -5,5 +7,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/redditclone'
+  },
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL'+'?ssl=true'
+
   }
 };
